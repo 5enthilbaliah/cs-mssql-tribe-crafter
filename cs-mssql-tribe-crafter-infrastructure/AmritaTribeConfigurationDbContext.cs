59e3657f9db24d768b5267d8b55f72cbd9da7d2d
5enthilbaliah <ternaryoperator.fork@gmail.com>
2023-08-20 16:45:34 -0500

    Build success

---
+++ cs-mssql-tribe-crafter-infrastructure/AmritaTribeConfigurationDbContext.cs
@@ -0,0 +1,20 @@
+﻿namespace AmritaDb.Tribe.Infrastructure;
+
+using Duende.IdentityServer.EntityFramework.DbContexts;
+
+using Microsoft.EntityFrameworkCore;
+
+public class AmritaTribeConfigurationDbContext : ConfigurationDbContext<AmritaTribeConfigurationDbContext>
+{
+    public AmritaTribeConfigurationDbContext(DbContextOptions<AmritaTribeConfigurationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder
+            .ApplyConfigurationsFromAssembly(typeof(AmritaTribeConfigurationDbContext).Assembly);
+    }
+}
--- cs-mssql-tribe-crafter-infrastructure/InfrastructureModule.cs
+++ cs-mssql-tribe-crafter-infrastructure/InfrastructureModule.cs
@@ -2,6 +2,14 @@
 
 using Autofac;
 
+using Duende.IdentityServer.EntityFramework.Interfaces;
+using Duende.IdentityServer.EntityFramework.Options;
+using Duende.IdentityServer.EntityFramework.Services;
+using Duende.IdentityServer.EntityFramework.Stores;
+using Duende.IdentityServer.Hosting.DynamicProviders;
+using Duende.IdentityServer.Services;
+using Duende.IdentityServer.Stores;
+
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
@@ -9,12 +17,35 @@ public class InfrastructureModule : Module
 {
     protected override void Load(ContainerBuilder builder)
     {
+        // ConfigurationStore
+        var options = new ConfigurationStoreOptions { ClientGrantType = new TableConfiguration("client_grant_type") };
+        builder.Register(_ => options).SingleInstance();
+        builder.RegisterType<ClientStore>()
+            .InstancePerDependency();
+        builder.RegisterType<ValidatingClientStore<ClientStore>>().As<IClientStore>()
+            .InstancePerDependency();
+
+        builder.RegisterType<ResourceStore>().As<IResourceStore>()
+            .InstancePerDependency();
+
+        builder.RegisterType<CorsPolicyService>().As<ICorsPolicyService>()
+            .InstancePerDependency();
+
+        builder.RegisterType<IdentityProviderStore>()
+            .InstancePerDependency();
+        builder.RegisterType<ValidatingIdentityProviderStore<IdentityProviderStore>>()
+            .InstancePerDependency();
+        builder.RegisterType<NonCachingIdentityProviderStore<ValidatingIdentityProviderStore<IdentityProviderStore>>>()
+            .As<IIdentityProviderStore>()
+            .InstancePerDependency();
+        
+        
         builder.Register(context =>
         {
             var config = context.Resolve<IConfiguration>();
-            var optionsBuilder = new DbContextOptionsBuilder<AmritaTribeDbContext>();
+            var optionsBuilder = new DbContextOptionsBuilder<AmritaTribeConfigurationDbContext>();
             optionsBuilder.UseSqlServer(config.GetConnectionString("AmritaTribeDb"));
-            return new AmritaTribeDbContext(optionsBuilder.Options);
-        }).InstancePerLifetimeScope();
+            return new AmritaTribeConfigurationDbContext(optionsBuilder.Options);
+        }).As<IConfigurationDbContext>().InstancePerDependency();
     }
 }
