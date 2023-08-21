4834a4ce6393d2f915ce54cc4268aac3f8840964
5enthilbaliah <ternaryoperator.fork@gmail.com>
2023-08-20 21:12:10 -0500

    ClientProperty spec

---
+++ cs-mssql-tribe-crafter-infrastructure/Specifications/ClientPropertySpecifications.cs
@@ -0,0 +1,21 @@
+﻿namespace AmritaDb.Tribe.Infrastructure.Specifications;
+
+using Duende.IdentityServer.EntityFramework.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class ClientPropertySpecifications : IEntityTypeConfiguration<ClientProperty>
+{
+    public void Configure(EntityTypeBuilder<ClientProperty> builder)
+    {
+        builder.Property(clientGrantType => clientGrantType.Id)
+            .HasColumnName("id");
+        builder.Property(clientGrantType => clientGrantType.Key)
+            .HasColumnName("key");
+        builder.Property(clientGrantType => clientGrantType.Value)
+            .HasColumnName("value");
+        builder.Property(clientGrantType => clientGrantType.ClientId)
+            .HasColumnName("client_id");
+    }
+}
