40549f31b5ba79777e862ffc8a10ea69b23d613e
5enthilbaliah <ternaryoperator.fork@gmail.com>
2023-08-20 20:51:11 -0500

    ClientIdPRestriction specs

---
+++ cs-mssql-tribe-crafter-infrastructure/Specifications/ClientClaimSpecifications.cs
@@ -0,0 +1,22 @@
+﻿namespace AmritaDb.Tribe.Infrastructure.Specifications;
+
+using Duende.IdentityServer.EntityFramework.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+public class ClientClaimSpecifications : IEntityTypeConfiguration<ClientClaim>
+{
+    public void Configure(EntityTypeBuilder<ClientClaim> builder)
+    {
+        builder.Property(clientGrantType => clientGrantType.Id)
+            .HasColumnName("id");
+        builder.Property(clientGrantType => clientGrantType.Type)
+            .HasColumnName("type");
+        builder.Property(clientGrantType => clientGrantType.ClientId)
+            .HasColumnName("client_id");
+        builder.Property(clientGrantType => clientGrantType.Value)
+            .HasColumnName("value");
+    }
+}
---
+++ cs-mssql-tribe-crafter-infrastructure/Specifications/ClientIdPRestrictionSpecifications.cs
@@ -0,0 +1,19 @@
+﻿namespace AmritaDb.Tribe.Infrastructure.Specifications;
+
+using Duende.IdentityServer.EntityFramework.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class ClientIdPRestrictionSpecifications : IEntityTypeConfiguration<ClientIdPRestriction>
+{
+    public void Configure(EntityTypeBuilder<ClientIdPRestriction> builder)
+    {
+        builder.Property(clientGIdPRestriction => clientGIdPRestriction.Id)
+            .HasColumnName("id");
+        builder.Property(clientGIdPRestriction => clientGIdPRestriction.Provider)
+            .HasColumnName("provider");
+        builder.Property(clientGIdPRestriction => clientGIdPRestriction.ClientId)
+            .HasColumnName("client_id");
+    }
+}
