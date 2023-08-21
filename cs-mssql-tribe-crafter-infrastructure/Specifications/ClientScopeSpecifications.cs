d7c7189c23d8f0b7325b734c8e9a0207f65cf06e
5enthilbaliah <ternaryoperator.fork@gmail.com>
2023-08-20 20:21:52 -0500

    ClientScope spec

---
+++ cs-mssql-tribe-crafter-infrastructure/Specifications/ClientScopeSpecifications.cs
@@ -0,0 +1,19 @@
+﻿namespace AmritaDb.Tribe.Infrastructure.Specifications;
+
+using Duende.IdentityServer.EntityFramework.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class ClientScopeSpecifications : IEntityTypeConfiguration<ClientScope>
+{
+    public void Configure(EntityTypeBuilder<ClientScope> builder)
+    {
+        builder.Property(client => client.Id)
+            .HasColumnName("id");
+        builder.Property(client => client.ClientId)
+            .HasColumnName("client_id");
+        builder.Property(client => client.Scope)
+            .HasColumnName("scope");
+    }
+}
