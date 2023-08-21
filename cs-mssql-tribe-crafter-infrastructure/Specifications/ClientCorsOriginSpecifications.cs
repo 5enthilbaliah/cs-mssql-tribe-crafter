6b30471cfa7fd4bf064ce800bbe3347434bba8cb
5enthilbaliah <ternaryoperator.fork@gmail.com>
2023-08-20 20:58:36 -0500

    ClientCorsOrigin spec

---
+++ cs-mssql-tribe-crafter-infrastructure/Specifications/ClientCorsOriginSpecifications.cs
@@ -0,0 +1,19 @@
+﻿namespace AmritaDb.Tribe.Infrastructure.Specifications;
+
+using Duende.IdentityServer.EntityFramework.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class ClientCorsOriginSpecifications : IEntityTypeConfiguration<ClientCorsOrigin>
+{
+    public void Configure(EntityTypeBuilder<ClientCorsOrigin> builder)
+    {
+        builder.Property(clientGrantType => clientGrantType.Id)
+            .HasColumnName("id");
+        builder.Property(clientGrantType => clientGrantType.Origin)
+            .HasColumnName("origin");
+        builder.Property(clientGrantType => clientGrantType.ClientId)
+            .HasColumnName("client_id");
+    }
+}
