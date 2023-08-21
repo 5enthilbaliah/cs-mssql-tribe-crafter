debd09dbd1ad05f5ce4c716c952b105d53799f9f
5enthilbaliah <ternaryoperator.fork@gmail.com>
2023-08-20 20:19:50 -0500

    ClientPostLogoutRedirectUri definitions

---
+++ cs-mssql-tribe-crafter-infrastructure/Specifications/ClientPostLogoutRedirectUriSpecifications.cs
@@ -0,0 +1,19 @@
+﻿namespace AmritaDb.Tribe.Infrastructure.Specifications;
+
+using Duende.IdentityServer.EntityFramework.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class ClientPostLogoutRedirectUriSpecifications : IEntityTypeConfiguration<ClientPostLogoutRedirectUri>
+{
+    public void Configure(EntityTypeBuilder<ClientPostLogoutRedirectUri> builder)
+    {
+        builder.Property(clientPostRedirectUri => clientPostRedirectUri.Id)
+            .HasColumnName("id");
+        builder.Property(clientPostRedirectUri => clientPostRedirectUri.PostLogoutRedirectUri)
+            .HasColumnName("post_logout_redirect_uri");
+        builder.Property(clientPostRedirectUri => clientPostRedirectUri.ClientId)
+            .HasColumnName("client_id");
+    }
+}
