baa3006f2f4011aebbd90a7301f184649c48235d
5enthilbaliah <ternaryoperator.fork@gmail.com>
2023-08-20 22:28:52 -0500

    IdentityProvider spec

---
+++ cs-mssql-tribe-crafter-infrastructure/Specifications/IdentityProviderSpecifications.cs
@@ -0,0 +1,33 @@
+﻿namespace AmritaDb.Tribe.Infrastructure.Specifications;
+
+using Duende.IdentityServer.EntityFramework.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class IdentityProviderSpecifications : IEntityTypeConfiguration<IdentityProvider>
+{
+    public void Configure(EntityTypeBuilder<IdentityProvider> builder)
+    {
+        builder.Property(identityProvider => identityProvider.Id)
+            .HasColumnName("id");
+        builder.Property(identityProvider => identityProvider.Scheme)
+            .HasColumnName("scheme");
+        builder.Property(identityProvider => identityProvider.DisplayName)
+            .HasColumnName("display_name");
+        builder.Property(identityProvider => identityProvider.Enabled)
+            .HasColumnName("enabled");
+        builder.Property(identityProvider => identityProvider.Type)
+            .HasColumnName("type");
+        builder.Property(identityProvider => identityProvider.Properties)
+            .HasColumnName("properties");
+        builder.Property(identityProvider => identityProvider.Created)
+            .HasColumnName("created");
+        builder.Property(identityProvider => identityProvider.Updated)
+            .HasColumnName("updated");
+        builder.Property(identityProvider => identityProvider.LastAccessed)
+            .HasColumnName("last_accessed");
+        builder.Property(identityProvider => identityProvider.NonEditable)
+            .HasColumnName("non_editable");
+    }
+}
